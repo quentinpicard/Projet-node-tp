17a01c2bba8b3ad64148f20eee1ab2160da96f1d
QUENTIN PICARD (Etudiant) <picarque@ur.univ-rouen.fr>
2023-03-16 10:42:58 +0100

    mise en place du serveru node op

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ let express = require('express'); //charger le modules express
 let app = express();
 let port = 3000;
 
+app.use(express.static(__dirname+"/www"));
+
 app.listen(port, ()=>{
     console.log('Le serveur est en route');
     console.log(`Serveur listening at https://localhost:${port}`);
 })
 
 app.get('/', (req, res, next)=>{
-    res.send('Bonjour, regarde ce ratio!');
+    res.sendFile('/www/index.html');
 })
